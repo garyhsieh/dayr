f171847160f910fbb18f9d19d213dfbae140a6a6
garyhsieh <garyhsieh@gmail.com>
2014-06-11 15:12:47 -0700

    update

--- app/mailers/daily_sms.rb
+++ app/mailers/daily_sms.rb
@@ -2,6 +2,6 @@ class DailySms < ActionMailer::Base
   default from: "dayr.me@gmail.com"
 
   def daily_message(user, challenge_name)
-  	mail(to: "5102077950@txt.att.net", subject: "Today's Baby Dayr Challenge", body: challenge_name.to_s + ". Visit: www.dayr.me")
+  	mail(to: user.sms_address, subject: "Today's Baby Dayr Challenge", body: challenge_name.to_s + ". Visit: www.dayr.me")
   end
 end
