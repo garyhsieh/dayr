62f3e5636a33e95a6fa795a800d662f64fa2e5b5
garyhsieh <garyhsieh@gmail.com>
2014-06-27 16:57:43 -0700

    email addresses

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,11 +1,11 @@
 class CommentMailer < ActionMailer::Base
-  default from: "dayr.me@gmail.com"
+  default from: "dayrme@gmail.com"
 
   def comment_reply_email(user, comment_type, body)
     mail(to: user.email, subject: "You Received a Reply to Your " + comment_type, body: body)
   end
 
   def advice_message_email(body)
-    mail(to: "garyhsieh@gmail.com", subject: "A New Advice Question", body: body)
+    mail(to: "karnold@ingham.org;KMckinley@ingham.org;dayrme@gmail.com", subject: "A New Advice Question", body: body)
   end
 end
--- app/mailers/daily_sms.rb
+++ app/mailers/daily_sms.rb
@@ -1,5 +1,5 @@
 class DailySms < ActionMailer::Base
-  default from: "dayr.me@gmail.com"
+  default from: "dayrme@gmail.com"
 
   def daily_message(user, challenge_name)
   	mail(to: user.sms_address, subject: "Today's Baby Dayr Challenge", body: challenge_name.to_s + ". Visit: www.dayr.me")
